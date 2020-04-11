cf56789053a6da7bedc57ce734c2b804d60e38bb
Arun Menon <arun.menon@qut.edu.au>
2020-04-11 13:49:45 +1000

    Starter commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-table striped hover :items="items"></b-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      items: [
+        { productNumber: 40, productName: 'Dickerson', standardCost: '10.00', listPrice: '20.00' },
+        { productNumber: 21, productName: 'Larsen', standardCost: '10.00', listPrice: '20.00' },
+        { productNumber: 89, productName: 'Geneva', standardCost: '10.00' ,  listPrice: '20.00' },
+        { productNumber: 38, productName: 'Jami', standardCost: '10.00' ,  listPrice: '20.00' }
+      ]
+    }
   }
 }
 </script>
